8d0c154d6c53dffc64f8d0d2df6f65862c4880a7
eswi <me@teri.kr>
2019-12-27 15:39:28 +0900

    Initial Commit

---
+++ git_01/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  git_01
+//
+//  Created by 위의석 on 2019/12/27.
+//  Copyright © 2019 Wi's Works. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
